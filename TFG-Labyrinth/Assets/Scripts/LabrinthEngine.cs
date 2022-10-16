9d1ae8d205361079e4df3c9ec8f39d5a67b90242
Mikhail <musacheva@uoc.edu>
2022-10-16 22:27:09 +0200

    git clean 2

--- TFG-Labyrinth/Assets/Scripts/LabrinthEngine.cs
+++ TFG-Labyrinth/Assets/Scripts/LabrinthEngine.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LabrinthEngine : MonoBehaviour
+
+public class LabrinthEngine
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int lab_width = 10;
+    public int lab_height = 10;
+
+
+    //Span at which the random generator can generate numbers. Used for determining the random weight assigned to 
+    //each node of the labyrinth. 
+    public int rand_low = 0;
+    public int rand_high = 50;
+
+    public node[,] lab_matrix { get; }
+
+    public struct node
     {
+        public node(int x, int y, float weight)
+        {
+            X = x;
+            Y = y;
+
+            Wheight = weight;
+        }
+
+        public float Wheight { get; }
+
+        public int X { get; }
+        public int Y { get; }
+    
         
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public LabrinthEngine()
     {
-        
+        lab_matrix = new node[lab_width, lab_height];
+
+
+        for(int i=0; i < lab_width; i++)
+        {
+            for(int j=0; j< lab_height; j++)
+            {
+                lab_matrix[i, j] = new node(i, j, Random.Range(rand_low, rand_high));
+            }
+        }
+
+
     }
 }
